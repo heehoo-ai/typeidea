fe8f4bbf4db3ffbf978311e9259ea5e99ce8fd15
heehoo-ai <heehoo@qq.com>
2020-04-24 16:39:33 +0800

    4/24 添加双后台

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,20 +1,34 @@
+
+from typeidea.custom_site import custom_site
 from django.contrib import admin
 from django.utils.html import format_html
 from django.urls import reverse
 from .models import Category, Tag, Post
 from django.contrib.auth.models import User
+from .adminforms import PostAdminForm
+
 
 # Register your models here.
-@admin.register(Category)
+
+
+# class PostInline(admin.StackedInline):  # admin.TabularInline是横向排列的样式
+#     fields = ('title', 'desc', 'owner')
+#     extra = 1
+#     model = Post
+
+
+@admin.register(Category, site=custom_site)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'is_nav', 'owner', 'created_time', )
     fields = ('name', 'status', 'is_nav')
+    # inlines = [PostInline, ]
+
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(CategoryAdmin, self).save_model(request, obj, form, change)
 
 
-@admin.register(Tag)
+@admin.register(Tag, site=custom_site)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status', )
@@ -35,6 +49,7 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
             return queryset.filter(category_id=self.value())
         return queryset
 
+
 class OwnerFilter(admin.SimpleListFilter):
     """ 自定义过滤器只展示当前用户"""
     title = "作者"
@@ -50,9 +65,10 @@ class OwnerFilter(admin.SimpleListFilter):
         return queryset
 
 
-@admin.register(Post)
+@admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
     # 展示页面
+    form = PostAdminForm
     list_display = [
         'title',  'category',  'status', 'owner', 'created_time', 'operator'
     ]
@@ -60,24 +76,46 @@ class PostAdmin(admin.ModelAdmin):
     # list_filter = [CategoryOwnerFilter, OwnerFilter]
     list_filter = ['owner', 'category']
     search_fields = ['title', 'category__name']
-
+    filter_horizontal = ('tag',)
     actions_on_top = True
     actions_on_bottom = True
 
     # 编辑页面
     # save_on_top = True
-    fields = (
-        'category', 'title',
-        'desc',
-        'status',
-        'content',
-        'tag',
+    exclude = ('owner',)
+    # fields = ('category', 'title',  'desc',  'status',  'content', 'tag', )
+    fieldsets = (
+        (
+            "基础配置", {
+                'description': '*',
+                'fields': (
+                    ('title', 'category'),
+                    'status',
+                ),
+            }
+        ),
+        (
+            '内容', {
+                'fields': (
+                    'desc',
+                    'content',
+                )
+            }
+        ),
+        (
+            '额外信息', {
+                'classes': ('collapse',),
+                'fields': ('tag',),
+            }
+        )
+
     )
+
 
     def operator(self, obj):
         return format_html(
             '<a href="{}">编辑</a>',
-            reverse('admin:blog_post_change', args=(obj.id,))
+            reverse('cus_admin:blog_post_change', args=(obj.id,))
 
         )
     operator.short_description = '操作'
@@ -92,3 +130,9 @@ class PostAdmin(admin.ModelAdmin):
         if request.user.is_superuser:
             return qs
         return qs.filter(owner=request.user)
+
+    class Media:
+        css = {
+            "all": ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css",)
+        }
+        js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js',)
